2310fca7340a6833faaebf46f57103b876694dd5
german heinz <heinz.german@gmail.com>
2023-11-29 09:19:39 -0600

    Adding Serializer

--- twitter-to-kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/KafkaProducerConfig.java
+++ twitter-to-kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/KafkaProducerConfig.java
@@ -38,7 +38,7 @@ public class KafkaProducerConfig <K extends Serializable, V extends SpecificReco
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaProducerConfigData.getBatchSize());
         props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaProducerConfigData.getLingerMs());
         props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType());
-        props.put(ProducerConfig.ACKS_CONFIG, kafkaProducerConfigData.getCompressionType());
+        props.put(ProducerConfig.ACKS_CONFIG, kafkaProducerConfigData.getAcks());
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerConfigData.getRequestTimeoutMs());
         props.put(ProducerConfig.RETRIES_CONFIG, kafkaProducerConfigData.getRetryCount());
 
