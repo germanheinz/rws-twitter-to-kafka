027ca5d1048523a01548072feeaa57e6d3d874b8
german heinz <heinz.german@gmail.com>
2023-12-06 04:08:23 -0600

    kafka-consumer config

--- twitter-to-kafka-config/src/main/java/com/microservices/demo/twitter/to/kafka/config/KafkaConfigData.java
+++ twitter-to-kafka-config/src/main/java/com/microservices/demo/twitter/to/kafka/config/KafkaConfigData.java
@@ -17,4 +17,60 @@ public class KafkaConfigData {
     private List<String> topicNamesToCreate;
     private Integer numOfPartitions;
     private Short replicationFactor;
+
+    public String getBootstrapServers() {
+        return bootstrapServers;
+    }
+
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    public String getSchemaRegistryUrlKey() {
+        return schemaRegistryUrlKey;
+    }
+
+    public void setSchemaRegistryUrlKey(String schemaRegistryUrlKey) {
+        this.schemaRegistryUrlKey = schemaRegistryUrlKey;
+    }
+
+    public String getSchemaRegistryUrl() {
+        return schemaRegistryUrl;
+    }
+
+    public void setSchemaRegistryUrl(String schemaRegistryUrl) {
+        this.schemaRegistryUrl = schemaRegistryUrl;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public List<String> getTopicNamesToCreate() {
+        return topicNamesToCreate;
+    }
+
+    public void setTopicNamesToCreate(List<String> topicNamesToCreate) {
+        this.topicNamesToCreate = topicNamesToCreate;
+    }
+
+    public Integer getNumOfPartitions() {
+        return numOfPartitions;
+    }
+
+    public void setNumOfPartitions(Integer numOfPartitions) {
+        this.numOfPartitions = numOfPartitions;
+    }
+
+    public Short getReplicationFactor() {
+        return replicationFactor;
+    }
+
+    public void setReplicationFactor(Short replicationFactor) {
+        this.replicationFactor = replicationFactor;
+    }
 }
