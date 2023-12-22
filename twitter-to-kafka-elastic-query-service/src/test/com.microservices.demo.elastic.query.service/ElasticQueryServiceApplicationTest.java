ac25a8c3437a5d3b94eda036b16366a6bc7b69f6
germanheinz <heinz.german@gmail.com>
2023-12-22 14:07:01 -0600

    Elastic Query Service Microservice

---
+++ twitter-to-kafka-elastic-query-service/src/test/com.microservices.demo.elastic.query.service/ElasticQueryServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.elastic.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+public class ElasticQueryServiceApplicationTest {
+
+
+    @Test
+    public void contextLoads(){}
+
+}
